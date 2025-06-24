336536cb5de97b9ccd1417c4ac153096db43d805
Gabriel Teiga <gabrielteiga99@gmail.com>
2025-06-24 18:57:33 -0300

    fix: main controller using request dto to handle body info

--- src/main/java/teiga/gabriel/grafos/app/controller/MainController.java
+++ src/main/java/teiga/gabriel/grafos/app/controller/MainController.java
@@ -3,6 +3,7 @@ package teiga.gabriel.grafos.app.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import teiga.gabriel.grafos.app.dto.ApiRequest;
@@ -12,7 +13,7 @@ import teiga.gabriel.grafos.app.dto.ApiResponse;
 @RequestMapping("/api/graph")
 public class MainController {
     @PostMapping
-    ResponseEntity<ApiResponse> getCaseResult(ApiRequest request){
+    ResponseEntity<ApiResponse> getCaseResult(@RequestBody ApiRequest request){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(
                         new ApiResponse(request.caso(), "Tstes")
